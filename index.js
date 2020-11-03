3f0e55d236b33bc1c6c24f40186f9d0a63108a2f
Shirley Ramos_M <srm.shirley@gmail.com>
2020-11-03 03:39:45 -0500

    tests

--- index.js
+++ index.js
@@ -1,56 +1,71 @@
-const path = require ('path');
-const absolutePath ='C:\\Users\\Shirley\\Desktop\\Projects LIM013\\Projects-FE-LIM013\\LIM013-fe-md-links\\README.md';
-const relativePath = './README.md';
-// console.log(path.isAbsolute(relativePath));
-// path.isAbsolute('C:\\Users\\Shirley\\Desktop\\Projects LIM013\\Projects-FE-LIM013\\LIM013-fe-md-links\\README.md');
+const path = require('path'); // módulo que nos ayuda a interactucar con paths y files
+const fs = require('fs'); // módulo para trabajar con el sistema de archivos
+// const jsdom = require ( "jsdom" ); para recorrer archivos
+// const { JSDOM } =  jsdom;
 
+// const pathRel = './README.md';
+// const pathAb =  'C:\\Users\\Shirley\\Desktop\\Projects LIM013\\LIM013-fe-md-links\\README.md';
+// const pathC = 'C:\Users\Shirley\Desktop\Projects LIM013\LIM013-fe-md-links\example.md';
 
-// Validar si la ruta es absoluta:
-const validatePathIsAbsolute = (ruta) => {
-  const result = path.isAbsolute(ruta);
-  return result
-};
+// Función para validar si existe ruta
+const isApath = (ruta) => fs.existsSync(ruta);
+
+// Función para validar si la ruta es absoluta:
+// const validatePathIsAbsolute = (ruta) => {
+//   const result = path.isAbsolute(ruta);
+//   return result
+// }
+
 // console.log(validatePathIsAbsolute(relativePath));
-
 
 // Transformar path relativa a absoluta:
 // const relativeToAbsolute = path.resolve('./README.md');
 // console.log(relativeToAbsolute);
 
-
-//Función que convierte cualquier ruta relativa a absoluta
-const relativeToAbsolute = (ruta) => {
-const absPath = path.resolve(ruta);
-// console.log(path.resolve(ruta));
+// Función que convierte ruta relativa a absoluta
+const convertPath = (ruta) => {
+  let absPath
+  const cwd = process.cwd();
+  if (!path.isAbsolute(ruta)) {
+  return path.join(cwd,ruta)
+  } else {
+    absPath = ruta;
+  }
   return absPath;
+}
+
+// Función para validar si es file
+const isFile = (ruta) => fs.statSync(ruta).isFile();
+
+// Función para validar si es directory
+const isDirectory = (ruta) => {
+  let pathDirectory = false;
+  if (path.parse(ruta).ext === '') {
+    pathDirectory = true;
+  }
+  return pathDirectory;
 };
-// console.log(relativeToAbsolute('./mdlinks.js'));
-
-
-// Validar si es archivo o directorio
-const fs = require ('fs');
-const stats = fs.statSync('C:\\Users\\Shirley\\Desktop\\Projects LIM013\\Projects-FE-LIM013\\LIM013-fe-md-links\\README.md');
-// console.log(' is directory? ' + stats.isDirectory ('C:\\Users\\Shirley\\Desktop\\Projects LIM013\\Projects-FE-LIM013\\LIM013-fe-md-links\\README.md'));
-// console.log(' is file? ' + stats.isFile ('C:\\Users\\Shirley\\Desktop\\Projects LIM013\\Projects-FE-LIM013\\LIM013-fe-md-links\\README.md'));
-
 
 // Función para validar si es MD
-const isMd = (mdPath) => {
-  const validateMd = path.parse(mdPath).ext === '.md';
-    return validateMd
-  };
-  //console.log(' is Md? ' + isMd ('C:\\Users\\Shirley\\Desktop\\Projects LIM013\\Projects-FE-LIM013\\LIM013-fe-md-links\\README.md'));
+const isMd = (ruta) => {
+  let validateMd = false;
+  if (path.parse(ruta).ext === '.md') {
+    validateMd = true;
+  }
+  return validateMd;
+};
+// console.log(' is Md? ' + isMd ('C:\\Users\\Shirley\\Desktop\\Projects LIM013\\LIM013-fe-md-links\\README.md'));
 
+// Si es MD almacenar en un array
+let mdArr = ['C:\\Users\\Shirley\\Desktop\\Projects LIM013\\LIM013-fe-md-links\\README.md']
+// console.log(mdArr.length);
 
-  // Si es MD almacenar en un array
-  let mdArr = ['C:\\Users\\Shirley\\Desktop\\Projects LIM013\\Projects-FE-LIM013\\LIM013-fe-md-links\\README.md']
-  // console.log(mdArr.length);
+//Recorrer array
+mdArr.forEach(function (element, index) {
+  // console.log(element, index);
+});
 
+// const stats = fs.statSync('C:\\Users\\Shirley\\Desktop\\Projects LIM013\\LIM013-fe-md-links\\README.md');
+// console.log(' is directory? ' + stats.isDirectory ('C:\\Users\\Shirley\\Desktop\\Projects LIM013\\LIM013-fe-md-links\\README.md'));
 
-  //Recorrer array
-  mdArr.forEach(function (element, index) {
-    // console.log(element, index);
-  })
-
-
-
+module.exports = { isApath, convertPath, isFile, isDirectory };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,58 @@
-const mdLinks = require('../');
+const testPath = require('../index.js');
 
+const stats = 'C:\\Users\\Shirley\\Desktop\\Projects LIM013\\LIM013-fe-md-links\\README.md';
+const directory = '\\Users\\Shirley';
 
-describe('mdLinks', () => {
+//Test para probar si path es válido
+// describe('Debería evaluar si el path existe', () => {
+//   it('should be a function', () => {
+//     expect(typeof testPath.isApath).toBe('function');
+//   });
+//   it('should return true if path is valid', () => {
+//     expect(testPath.isApath('.\\README.md')).toBe(true);
+//   });
+//   it('should return false if path is not valid', () => {
+//     expect(testPath.isApath('C:\\Users\\Shirley\\House')).toBe(false);
+//   });
+// });
 
-  it('should...', () => {
-    console.log('FIX ME!');
+// Test para la función que convierte ruta relativa a absoluta
+describe('Debería validar si es una ruta', () => {
+  const pathAbsolute = 'C:\\Users\\Shirley\\Desktop\\Projects LIM013\\LIM013-fe-md-links\\README.md';
+  it('should be a function', () => {
+    expect(typeof testPath.convertPath).toBe('function');
   });
+  it('should return an absolute path', () => {
+    expect(testPath.convertPath(pathAbsolute)).toBe(pathAbsolute);
+  });
+  it('should convert a relative path to an absolute path', () => {
+    const relativePath = './README.md';
+    expect(testPath.convertPath(relativePath)).toBe(pathAbsolute);
+  });
+});
 
+//Test para validar si es file
+describe('should validate if the path is a file', () =>{
+  it('should validate if its a function', () => {
+    expect(typeof testPath.isFile).toBe('function');
+  });
+  it('should return true if its a file',() => {
+    expect(testPath.isFile(stats)).toEqual(true);
+  });
+  it('should return false if its a directory', () => {
+    expect(testPath.isFile(directory)).toEqual(false);
+  });
+});
+
+//Test para validar si es directory
+describe('should validate if the path is a directory', () =>{
+  it('should validate if its a function', () => {
+    expect(typeof testPath.isDirectory).toBe('function');
+  });
+  it('should return true if its a directory',() => {
+    expect(testPath.isDirectory(directory)).toEqual(true);
+  });
+  it('should return false if its a file', () => {
+    expect(testPath.isDirectory(stats)).toEqual(false);
+  });
 });
